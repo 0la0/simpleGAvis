ab27cd7c5ce34edd84fa4d5e4f1edb7c9e92ddb1
0la0 <luke.kansas@gmail.com>
2015-07-26 11:33:16 -0500

    2D animation driver cleanup

--- src/swingUiDriver/AnimationDriver.java
+++ src/swingUiDriver/AnimationDriver.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
@@ -13,7 +11,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -25,7 +22,6 @@ import javax.swing.Timer;
 import gaViz.fitness.FitnessCustomGoal;
 import gaViz.main.BinaryStringHelper;
 import gaViz.main.GaGenerator;
-import gaViz.main.Individual;
 import gaViz.main.GaConfigOptions;
 import gaViz.main.Population;
 import gaViz.main.PopulationComparator;
@@ -37,7 +33,6 @@ public class AnimationDriver {
 	private int goal;
 	private AnimationPanel animationPanel;
 	private Timer timer;
-	private int timerCnt = 1;
 	private BufferedImage img = null;
 	private Graphics2D imgGraphics = null;
 	private int animationStepTime = 17;
@@ -51,24 +46,30 @@ public class AnimationDriver {
 	private int prevHeight = 500;//---dimension of JFrame
 	private boolean renderGoal = true;
 	private GaGenerator gaGenerator;
+	private int imgWidth;
+	private int imgHeight;
 	
 	public AnimationDriver (GaConfigOptions options) {
 		this.options = options;
 		this.gaGenerator = new GaGenerator(options);
 		this.goal = this.gaGenerator.getGoalSize();
 		
+		this.imgWidth = BinaryStringHelper.maxVal;
+		this.imgHeight = BinaryStringHelper.maxVal;
+		if (this.options.numGenes == 3) {
+			this.imgWidth = (int) (this.options.populationSize * 2);
+			this.imgHeight = 1;
+		}
+		this.createBufferedImage(this.imgWidth, this.imgHeight);
+		
 		if (this.options.fitnessObj instanceof FitnessCustomGoal) {
 			this.isCustomGoal = true;
 		}
-		
 		this.buildAnimationFrame(500, 500);
-		
-		this.timer = new Timer(animationStepTime, new ActionListener() {
-	        public void actionPerformed(ActionEvent evt) {
-	    		gaGenerator.createNewPopulation();
-	        	generate();
-	        }
-	    });
+		this.timer = new Timer(animationStepTime, actionEvent -> {
+			gaGenerator.createNewPopulation();
+        	generate();
+		});
 	    this.timer.start();
 	}
 	
@@ -78,7 +79,6 @@ public class AnimationDriver {
 	}
 	
 	private void generate () {
-		
 		switch (this.options.numGenes) {
 		case 2:
 			this.animateXY();
@@ -96,8 +96,7 @@ public class AnimationDriver {
 				this.timer.stop();
 			} catch (NullPointerException e) {}
 		}
-		
-		if (this.timerCnt++ % 400 == 0) this.createNewGoal();
+		if (Math.random() < 0.005) this.createNewGoal();
 		//System.out.println("Total iteration time: " + ((System.nanoTime() - startTime) / 1000000.0) );
 	}
 	
@@ -105,17 +104,8 @@ public class AnimationDriver {
 	 * Two genes, mapped to x and y position in image plane
 	 */
 	private void animateXY () {
-		int w = BinaryStringHelper.maxVal;
-		int h = BinaryStringHelper.maxVal;
-		if (this.img == null) {
-			this.img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-			this.imgGraphics = this.img.createGraphics();
-			this.imgGraphics.setColor(Color.BLACK);
-			this.imgGraphics.fillRect(0,  0,  w,  h);
-		}
-		
 		this.imgGraphics.setColor(Color.BLACK);
-		this.imgGraphics.fillRect(0,  0,  w,  h);
+		this.imgGraphics.fillRect(0,  0, this.imgWidth,  this.imgHeight);
 		
 		if (this.isCustomGoal && this.renderGoal) {
 			this.imgGraphics.setPaint(new Color(100, 100, 255));
@@ -129,8 +119,8 @@ public class AnimationDriver {
 			double normalX = individual.getGene(0) / (goal * 1.0);
 			double normalY = individual.getGene(1) / (goal * 1.0);
 			
-			int x = (int) Math.floor(w * normalX);
-			int y = (int) Math.floor(h * normalY);
+			int x = (int) Math.floor(this.imgWidth * normalX);
+			int y = (int) Math.floor(this.imgHeight * normalY);
 			
 			this.imgGraphics.setPaint(new Color(255, 255, 255, 100));
 			this.imgGraphics.fillRect(x - 1, y - 1, 2, 2);
@@ -143,14 +133,6 @@ public class AnimationDriver {
 	 * Three genes - mapped as RGB, individuals are columns
 	 */
 	private void animateRGBcolumns () {
-		
-		if (this.img == null) {
-			int w = (int) (this.options.populationSize * 2);
-			int h = 1;
-			this.img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-			this.imgGraphics = this.img.createGraphics();
-		}
-		
 		Population renderPopulation = this.gaGenerator.getLatestPopulation();
 		renderPopulation.sort(this.populationSort);
 		AtomicInteger indexCnt = new AtomicInteger(0);
@@ -173,19 +155,9 @@ public class AnimationDriver {
 	 * Five genes, mapped to RGB, then x and y position in image plane
 	 */
 	private void animateRGBXY () {
-		int w = BinaryStringHelper.maxVal;
-		int h = BinaryStringHelper.maxVal;
-		//int opacityMult = 20;
-		if (this.img == null) {
-			this.img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-			this.imgGraphics = this.img.createGraphics();
-			this.imgGraphics.setColor(Color.BLACK);
-			this.imgGraphics.fillRect(0,  0,  w,  h);
-		}
-		
 		//---CLEAR BUFFERED IMAGE TO BLACK---//
 		this.imgGraphics.setColor(Color.BLACK);
-		this.imgGraphics.fillRect(0,  0,  w,  h);
+		this.imgGraphics.fillRect(0,  0,  this.imgWidth,  this.imgHeight);
 		
 		//---RENDER GOAL STATE---//
 		if (this.isCustomGoal && this.renderGoal) {
@@ -193,13 +165,11 @@ public class AnimationDriver {
 			this.imgGraphics.setPaint(new Color(
 					goalPosition[0], goalPosition[1], goalPosition[2]));
 			this.imgGraphics.fillRect(goalPosition[3] - 5, goalPosition[4] - 5, 10, 10);
-			//opacityMult = 20;
 		}
 
 		AtomicInteger indexCnt = new AtomicInteger(0);
 		this.gaGenerator.getGenerations().getPopulations().forEach(population -> {
 			
-			//int opacity = (10 * opacityMult) / indexCnt.incrementAndGet();
 			int opacity = getOpacity(indexCnt.getAndIncrement());
 			population.getIndividuals().stream().forEach(individual -> {
 				
@@ -210,8 +180,8 @@ public class AnimationDriver {
 				int red = (int) Math.floor(255 * normalVals[0]);
 				int green = (int) Math.floor(255 * normalVals[1]);
 				int blue = (int) Math.floor(255 * normalVals[2]);
-				int x = (int) Math.floor(w * normalVals[3]);
-				int y = (int) Math.floor(h * normalVals[4]);
+				int x = (int) Math.floor(this.imgWidth * normalVals[3]);
+				int y = (int) Math.floor(this.imgHeight * normalVals[4]);
 				
 				
 				this.imgGraphics.setPaint(new Color(red, green, blue, opacity));
@@ -225,6 +195,13 @@ public class AnimationDriver {
 	
 	private int getOpacity (int index) {
 		return 220 / (this.options.numGenerations - index);
+	}
+	
+	private void createBufferedImage (int width, int height) {
+		this.img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		this.imgGraphics = this.img.createGraphics();
+		this.imgGraphics.setColor(Color.BLACK);
+		this.imgGraphics.fillRect(0,  0,  width,  height);
 	}
 	
 	private void buildAnimationFrame (int w, int h) {
